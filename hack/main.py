0361339e5d2f2b447b95cadc1c0884b9af03623d
thegamebegins25 <wdppostcard@gmail.com>
2022-01-08 15:52:58 -0800

    Update main.py

--- hack/main.py
+++ hack/main.py
@@ -10,7 +10,7 @@ repo = requests.get("https://raw.githubusercontent.com/thegamebegins25/blooketha
 module = __import__(__name__)
 file = open(module.__file__, "r")
 if file.read() != repo.text:
-    print("Update available. Please open the 'UpdateBlooketHack' file.")  
+    print("Update available. Please open the 'update.py' file.")  
     quit()
 
 
