4a42a52941968b7df413a6db30b0df79b4a74d1a
thegamebegins25 <wdppostcard@gmail.com>
2022-01-09 15:36:16 -0800

    added stopping update
    also fixed versioning

--- hack/update.py
+++ hack/update.py
@@ -1,4 +1,4 @@
-#v2.1.0
+#v1.1.1
 import requests
 
 
@@ -9,26 +9,33 @@ repo = requests.get("https://raw.githubusercontent.com/thegamebegins25/blooketha
 
 file = open("main.py", "r")
 if file.read() != repo.text:
-    print("Hack is not updated. Updating file.")
-    file3 = open("main.py", "rU")
-    lines1 = file3.readlines()
-    file3.close()
+    print("Hack is not updated. Do you want to update? (y or n)")
+    answer = input()
+    if answer == "y":
 
-    try:
-        email = lines1[5]
-        password = lines1[6]
-    except:
-        print("No email or password. You will have to re-enter it.")
-    print(email + password)
-    file2 = open("main.py", "w")
-    lines = repo.text.split("\n")
+        file3 = open("main.py", "rU")
+        lines1 = file3.readlines()
+        file3.close()
+
+        try:
+            email = lines1[5]
+            password = lines1[6]
+        except:
+            print("No email or password. You will have to re-enter it.")
+        print(email + password)
+        file2 = open("main.py", "w")
+        lines = repo.text.split("\n")
     
-    lines[5] = email
-    lines[6] = password
+        lines[5] = email
+        lines[6] = password
     
-    file2.writelines(lines)
-    file2.close()
-    print("Hack updated. Please reload your python interpreter.")
-file.close()
+        file2.writelines(lines)
+        file2.close()
+        print("Hack updated. Please reload your python interpreter.")
+    else:
+        print("Ok. Make sure to update soon for the most recent bug patches and feature updates.")
+    file.close()
+    
+
 
 
