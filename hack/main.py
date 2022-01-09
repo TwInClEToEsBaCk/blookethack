021aa9e57221dd9e77c8e5d1f80481692b7204f9
thegamebegins25 <wdppostcard@gmail.com>
2022-01-09 15:45:46 -0800

    Update main.py

--- hack/main.py
+++ hack/main.py
@@ -33,8 +33,6 @@ if file.read()[:7] != repo.text[:7]:
     print("Update available. Updating now...")
     import update
     time.sleep(3)
-    print("Update complete. The program will now stop for the updates to process.")
-    quit()
 
 
 
